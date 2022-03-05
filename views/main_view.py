1ac16707c5187f357b994d8cf4036c120935f2a8
MOUSSA AMRIOUN <83790916+MAM95190@users.noreply.github.com>
2022-03-05 21:23:11 +0100

    Update main_view.py

--- views/main_view.py
+++ views/main_view.py
@@ -1,40 +1,42 @@
-# -*- coding: utf-8 -*-
 import inquirer
 from enum import Enum
 from views.player.player_view import player_prompt
 from views.tournament.tournament_view import tournament_prompt
 from constants.common_constants import ANSWER_KEY, SOMETHING_UNEXPECTED_STR
 
+
 class Answer(Enum):
-  """Possible answers for this prompt"""
-  EXIT = 0
-  PLAYER = 1
-  TOURNAMENT = 2
+    """Possible answers for this prompt"""
+    EXIT = 0
+    PLAYER = 1
+    TOURNAMENT = 2
+
 
 answers_list = {
-  Answer.EXIT: "Exit",
-  Answer.PLAYER: "Manage players",
-  Answer.TOURNAMENT: "Manage tournaments"
+    Answer.EXIT: "Exit",
+    Answer.PLAYER: "Manage players",
+    Answer.TOURNAMENT: "Manage tournaments"
 }
+
 
 def prompt():
     """Show the main prompt."""
     continue_prompt = True
 
     while (continue_prompt):
-      answer = main_question()
+        answer = main_question()
 
-      if (answer[ANSWER_KEY] == answers_list[Answer.EXIT]):
-        continue_prompt = False
-      
-      elif (answer[ANSWER_KEY] == answers_list[Answer.PLAYER]):
-        player_prompt()
+        if (answer[ANSWER_KEY] == answers_list[Answer.EXIT]):
+            continue_prompt = False
 
-      elif (answer[ANSWER_KEY] == answers_list[Answer.TOURNAMENT]):
-        tournament_prompt()
+        elif (answer[ANSWER_KEY] == answers_list[Answer.PLAYER]):
+            player_prompt()
 
-      else:
-        print(SOMETHING_UNEXPECTED_STR)
+        elif (answer[ANSWER_KEY] == answers_list[Answer.TOURNAMENT]):
+            tournament_prompt()
+
+        else:
+            print(SOMETHING_UNEXPECTED_STR)
 
 
 def main_question():
@@ -43,11 +45,11 @@ def main_question():
     answer = inquirer.prompt([
         inquirer.List(
             ANSWER_KEY,
-            message = "General: What do you want to do?",
-            choices = [
-              answers_list[Answer.TOURNAMENT], 
-              answers_list[Answer.PLAYER], 
-              answers_list[Answer.EXIT]
+            message="General: What do you want to do?",
+            choices=[
+                answers_list[Answer.TOURNAMENT],
+                answers_list[Answer.PLAYER],
+                answers_list[Answer.EXIT]
             ],
             carousel=True,
         )
