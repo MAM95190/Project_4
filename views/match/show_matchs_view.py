467cfd18341f072efe55ca50550a7bbf00a3b04a
MOUSSA AMRIOUN <83790916+MAM95190@users.noreply.github.com>
2022-03-07 00:55:55 +0100

    Update show_matchs_view.py

--- views/match/show_matchs_view.py
+++ views/match/show_matchs_view.py
@@ -1,9 +1,7 @@
 import inquirer
 from enum import Enum
 from constants.common_constants import ANSWER_KEY
-
 from controllers.player_controller import get_player_from_id
-from views.match.single_match_view import single_match_prompt
 from views.match.enter_score_player import enter_score_player_prompt
 
 
@@ -58,14 +56,19 @@ def show_matchs_tournament_prompt(turn):
         for player in players_1:
             player_1 = players_1[index]
             score_1 = score_players_1[index]
-            player_1_info = f"{player_1.name} (elo: {player_1.elo})(score : {score_1})"
+            player_1_info = (f"{player_1.name}",
+                             f"(elo: {player_1.elo})",
+                             f"(score : {score_1})")
 
             player_2 = players_2[index]
             score_2 = score_players_2[index]
-            player_2_info = f"{player_2.name} (elo: {player_2.elo})(score : {score_2})"
+            player_2_info = (f"{player_2.name}",
+                             f"(elo: {player_2.elo})",
+                             f"(score : {score_2})")
 
             match = (
-                f"Match : {player_1_info} <|> {player_2_info}", f"{index}")
+                f"""Match : {player_1_info} <|> {player_2_info}""",
+                f"{index}")
             matchs_list.append(match)
 
             index = index + 1
@@ -89,25 +92,11 @@ def show_matchs_tournament_prompt(turn):
         else:
             print(answers)
             selected_match_index = int(answers['answer'])
-
             updated_match = enter_score_player_prompt(
                 turn,
                 selected_match_index,
                 players_1[selected_match_index],
                 players_2[selected_match_index],
             )
-
             if (updated_match):
                 continue_prompt = False
-
-            # 1. Extraire l'index du match sélectionné -> answers['answer']
-            # 2. Appeler la méthode `single_match_prompt(...)` (autre fichier)
-            #    avec en paramètre l'index du match sélectionné + tour (instance de classe),
-            #    (et peut-être le tournoi mais pas sûr)
-            # 3. Dans `single_match_promp(...)`, afficher un menu avec 'Back' et 'Update'
-            # 4. Le bouton 'Update' demande à l'utilisateur d'entrer les scores
-            #    en premier lieu pour le joueur 1, puis pour le joueur 2
-            # 5. Récupérer les valeurs saisies par l'utilisateur et créer un nouveau tuple
-            #    avec les anciennes infos (les joueurs, index de match) + les scores mis à jour.
-            # 6. Remplacer l'ancien tuple de match par le nouveau dans la liste (ou créer une nouvelle liste)
-            # 7. Enregistrer cette nouvelle liste de matchs en base de données.
