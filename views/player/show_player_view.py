9c4ddf7f710a746c3885c5ddae6dabe79cb61a68
MOUSSA AMRIOUN <83790916+MAM95190@users.noreply.github.com>
2022-03-07 00:59:15 +0100

    Update show_player_view.py

--- views/player/show_player_view.py
+++ views/player/show_player_view.py
@@ -1,6 +1,7 @@
 import inquirer
 from enum import Enum
-from controllers.player_controller import get_all_players_as_list, get_player_from_id
+from controllers.player_controller import get_all_players_as_list
+from controllers.player_controller import get_player_from_id
 from constants.common_constants import ANSWER_KEY
 from views.player.single_player_view import single_player_prompt
 
@@ -40,7 +41,6 @@ def show_player_prompt():
             player = get_player_from_id(answers
                                         [ANSWER_KEY])
 
-            #player = deserialize_player(player_data)
             show_single_player(player)
             single_player_prompt(player)
 
@@ -48,9 +48,10 @@ def show_player_prompt():
 def show_single_player(player):
     """Format & display a single player to the console."""
 
-    print("------")
     print(
-        f"• First name: {player.first_name}\n• Last name: {player.last_name}\n• Elo: {player.elo}\n• Sex: {player.sex}\n• Birth date: {player.birth_date}"
+        f"""• First name: {player.first_name}
+        \n• Last name: {player.last_name}
+        \n• Elo: {player.elo}
+        \n• Sex: {player.sex}
+        \n• Birth date: {player.birth_date}"""
     )
-    print("------")
-    print("")
